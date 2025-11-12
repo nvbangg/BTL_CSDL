666b679dbe06459ea4cdb3a1cb9ef3c8f275f950
Nguyen Xuan Dat <datn28102005@gmail.com>
2025-11-12 23:10:14 +0700

    add

--- truy_van/12.sql
+++ truy_van/12.sql
@@ -1,7 +1,7 @@
- -- 12. Tìm lớp học có sĩ số lớn hơn 30 học sinh.
+ -- 12. Tìm lớp học có sĩ số lớn hơn 15 học sinh.
  SELECT hs.TenNamHoc, hs.TenLop, COUNT(*) AS SiSo
  FROM HocSinh hs
  WHERE hs.TrangThaiHocTap = 'Đang học'
  GROUP BY hs.TenNamHoc, hs.TenLop
- HAVING COUNT(*) > 30
+ HAVING COUNT(*) > 15
  ORDER BY hs.TenNamHoc, hs.TenLop;
