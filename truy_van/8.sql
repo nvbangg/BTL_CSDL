cbffe817ce0c33ba457a3ac9ca85627386b0fbf2
dantiu147 <tiudan147@gmail.com>
2025-11-19 00:36:06 +0700

    cau8

--- truy_van/8.sql
+++ truy_van/8.sql
@@ -1,1 +1,5 @@
 -- 8. Liệt kê lương các nhân sự trong tháng hiện tại
+select ns.MaNS, ns.HoTen, (bl.LuongCoBan + bl.Thuong + bl.PhuCap - bl.KhauTru) as TongLuong
+from nhansu as ns, bangluong as bl
+where ns.MaNS = bl.MaNS
+and month(curdate()) = month(bl.NgayNhanLuong);
