dcc3d376e9ec9709a685526c3d9a6928be5350f4
dantiu147 <tiudan147@gmail.com>
2025-11-19 00:42:27 +0700

    lam cau 8 cau 9

--- truy_van/8.sql
+++ truy_van/8.sql
@@ -2,4 +2,5 @@
 select ns.MaNS, ns.HoTen, (bl.LuongCoBan + bl.Thuong + bl.PhuCap - bl.KhauTru) as TongLuong
 from nhansu as ns, bangluong as bl
 where ns.MaNS = bl.MaNS
-and month(curdate()) = month(bl.NgayNhanLuong);
+and month(curdate()) = month(bl.NgayNhanLuong)
+and year(curdate()) = year(bl.NgayNhanLuong);
--- truy_van/9.sql
+++ truy_van/9.sql
@@ -1,1 +1,14 @@
--- 9. Liệt kê 5 nhân sự lương cao nhất hiện tại
+-- 8. Liệt kê lương các nhân sự trong tháng hiện tại
+SELECT 
+    ns.MaNS,
+    ns.HoTen,
+    (bl.LuongCoBan + bl.Thuong + bl.PhuCap - bl.KhauTru) AS TongLuong
+FROM
+    nhansu AS ns,
+    bangluong AS bl
+WHERE
+    ns.MaNS = bl.MaNS
+        AND MONTH(CURDATE()) = MONTH(bl.NgayNhanLuong)
+        AND YEAR(CURDATE()) = YEAR(bl.NgayNhanLuong)
+ORDER BY TongLuong DESC
+LIMIT 5;
