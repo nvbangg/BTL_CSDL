d03760e4d0631037ef3f76896736ffb5dfeecd51
NV Bằngg <nvbangg@gmail.com>
2025-11-19 00:56:51 +0700

    edit

--- truy_van/9.sql
+++ truy_van/9.sql
@@ -1,14 +1,11 @@
 -- 9. Liệt kê 5 nhân sự lương cao nhất hiện tại
-SELECT 
-    ns.MaNS,
+SELECT ns.MaNS,
     ns.HoTen,
     (bl.LuongCoBan + bl.Thuong + bl.PhuCap - bl.KhauTru) AS TongLuong
-FROM
-    nhansu AS ns,
+FROM nhansu AS ns,
     bangluong AS bl
-WHERE
-    ns.MaNS = bl.MaNS
-        AND MONTH(CURDATE()) = MONTH(bl.NgayNhanLuong)
-        AND YEAR(CURDATE()) = YEAR(bl.NgayNhanLuong)
+WHERE ns.MaNS = bl.MaNS
+    AND MONTH(CURDATE()) = MONTH(bl.NgayNhanLuong)
+    AND YEAR(CURDATE()) = YEAR(bl.NgayNhanLuong)
 ORDER BY TongLuong DESC
 LIMIT 5;
