e47448c608bf40c661cd82f274be581f25e2f83d
Nguyen Xuan Dat <datn28102005@gmail.com>
2025-11-19 09:01:15 +0700

    them c16, update c20

--- truy_van/16.sql
+++ truy_van/16.sql
@@ -1,1 +1,7 @@
--- 16. Tính tổng số tiền đã thu trong năm
+-- 16. Tính tổng số tiền đã thu trong năm '2025-2026'
+SELECT 
+    TenNamHoc,
+    SUM(SoTienDaDong) AS TongTienThu
+FROM LanDongPhi
+WHERE TenNamHoc = '2025-2026'
+GROUP BY TenNamHoc;
--- truy_van/20.sql
+++ truy_van/20.sql
@@ -1,24 +1,28 @@
 -- 20. Liệt kê các sinh viên nợ học phí hiện tại
+WITH TongPhi AS (
+    SELECT SUM(SoTienPhaiDong) AS TongPhaiDong
+    FROM KhoanPhiTrongNam
+    WHERE TenNamHoc = '2025-2026'
+),
+DaDong AS (
+    SELECT MaHS, TenLop, TenNamHoc, SUM(SoTienDaDong) AS DaDong
+    FROM LanDongPhi
+    WHERE TenNamHoc = '2025-2026'
+    GROUP BY MaHS, TenLop
+)
 
-SELECT T1.MaHS, T1.TenLop, T1.TenNamHoc,
-    IFNULL(T2.ConNo,
-            (SELECT SUM(KP.SoTienPhaiDong)
-                FROM KhoanPhiTrongNam AS KP
-                WHERE KP.TenNamHoc = '2025-2026')) AS ConNo
-FROM
-    (SELECT MaHS, TenLop, TenNamHoc
-    FROM HocSinh
-    WHERE TenNamHoc = '2025-2026') AS T1
-LEFT JOIN
-    (SELECT MaHS, TenLop, TenNamHoc,
-            ((SELECT SUM(KP.SoTienPhaiDong)
-                FROM KhoanPhiTrongNam AS KP
-                WHERE KP.TenNamHoc = '2025-2026') - SUM(LDP.SoTienDaDong)) AS ConNo
-    FROM LanDongPhi AS LDP
-    GROUP BY TenNamHoc , TenLop , MaHS) T2 
-ON T1.MaHS = T2.MaHS
-AND T1.TenLop = T2.TenLop
-AND T1.TenNamHoc = T2.TenNamHoc
+SELECT 
+    HS.MaHS,
+    HS.TenLop,
+    HS.TenNamHoc,
+    (TP.TongPhaiDong - IFNULL(DD.DaDong, 0)) AS ConNo
+FROM HocSinh AS HS
+JOIN TongPhi TP
+LEFT JOIN DaDong DD
+    ON HS.MaHS = DD.MaHS
+    AND HS.TenLop = DD.TenLop
+    AND HS.TenNamHoc = DD.TenNamHoc
+WHERE HS.TenNamHoc = '2025-2026';
 
 
 
